b3b6396b6fe42d3d401e4cf84508648c6e07da80
Jason Britto <jason.britto@ark-energy.eu>
2021-06-09 17:59:20 +0100

    added border feature and fixed text overlap

--- src/components/ProductImageCarosel.js
+++ src/components/ProductImageCarosel.js
@@ -3,9 +3,25 @@ import ReactStars from "../components/react-stars";
 
 const ProductImageCarosel = ({data}) => {
     let images = data.images
-
     const [selectedImg, setSelectedImg] = useState(images[0])
     const [tempImg, setTempImg] = useState(null)
+    const [imgGrid, setImgGrid] = useState([
+
+        ...images.map((i, index) => (
+            <img 
+                key={index} 
+                src={i} 
+                alt={'Name'}
+                width="600" 
+                height="400"
+                onClick={(i) => changeImg(i,index)}
+                onMouseEnter={(i) => updateTempImg(i)}
+                onMouseLeave={() => updateTempImg(null)}
+                style={{border:'None'}}
+            />
+        ))
+
+    ])
 
     const updateTempImg = (img) => {
         if (img !== null) {
@@ -17,44 +33,65 @@ const ProductImageCarosel = ({data}) => {
             setTempImg(null)
     }
 
-    const changeImg = (img) => {
+    const changeImg = (img, idx) => {
+        setImgGrid([
+
+            ...images.map((i, index) => {
+                if(index === idx){
+                    console.log(i.props)
+                    return(<img 
+                        key={index} 
+                        src={i} 
+                        alt={'Name'}
+                        width="600" 
+                        height="400"
+                        onClick={(i) => changeImg(i,index)}
+                        onMouseEnter={(i) => updateTempImg(i)}
+                        onMouseLeave={() => updateTempImg(null)}
+                        style={{border:'3px solid #555'}}
+                    />)
+                }
+                else 
+                    return(<img 
+                        key={index} 
+                        src={i} 
+                        alt={'Name'}
+                        width="600" 
+                        height="400"
+                        onClick={(i) => changeImg(i,index)}
+                        onMouseEnter={(i) => updateTempImg(i)}
+                        onMouseLeave={() => updateTempImg(null)}
+                        style={{border:'None'}}
+                    />)
+
+            })
+        ])
+        
+
         let src = img.target.src
         setSelectedImg(src)
     }   
+
     return (
         <>
             <div className='display-image'>
                 <img 
                     src={tempImg === null ? selectedImg : tempImg} 
                     alt={data.name} 
-                    width="600" 
-                    height="400"
                 />
                 {/*To update star rating change starRating in shared>data.js*/}
-                <div className='product-rating'>
-                    <ReactStars
-                        size={1}
-                        value={data.starRating}
-                        edit={false}
-                        isHalf={ true }
-                    />
-                </div>
-                
+            <div className='product-rating'>
+                <ReactStars
+                    size={1}
+                    value={data.starRating}
+                    edit={false}
+                    isHalf={ true }
+                />
+            </div>
             </div>
             <div className='image-grid-container'>
                 <div className='image-grid'>
-                    {images.map((i, index) => (
-                        <img 
-                            key={index} 
-                            src={i} 
-                            alt={'Name'}
-                            width="600" 
-                            height="400"
-                            onClick={(i) => changeImg(i)}
-                            onMouseEnter={(i) => updateTempImg(i)}
-                            onMouseLeave={() => updateTempImg(null)}
-                        />
-                    ))} 
+                    {[...imgGrid]} 
                 </div>
             </div>
         </>
